bc3b0c82fe69f8c4e534825e43292dd9a153fdb0
Epicsiddharth <isonlysiddharth@gmail.com>
2023-03-10 02:31:55 +0530

    serv

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-var HTTP_PORT = process.env.port || 8080;
-var express = required("express");
+var HTTP_PORT = process.env.PORT || 8080;
+var express = require("express");
 var app = express();
 
+// setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
-  res.send("Hello from Noman Atique");
+  res.send("Hello World!");
 });
+
+// setup http server to listen on HTTP_PORT
 app.listen(HTTP_PORT);
