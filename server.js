efc573fd96f566f5d5f7ef3fd027fad653ba9553
Epicsiddharth <isonlysiddharth@gmail.com>
2023-03-10 02:33:41 +0530

    erro

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
-var HTTP_PORT = process.env.PORT || 8080;
-var express = require("express");
+var HTTP_PORT = process.env.port || 8080;
+var express = required("express");
 var app = express();
 
-// setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello from Noman Atique");
 });
-
-// setup http server to listen on HTTP_PORT
 app.listen(HTTP_PORT);
